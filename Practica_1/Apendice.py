59168db22308444eabc2f84ed26945a5d3a481cc
jcalcausal <jcalcausal@gmail.com>
2024-10-08 13:58:36 +0200

    Apendice

---
+++ Practica_1/Apendice.py
@@ -0,0 +1,35 @@
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import DES, AES
+from Crypto.Util.Padding import pad,unpad
+from Crypto.Util import Counter
+import base64
+
+# Datos necesarios
+key = get_random_bytes(8) # Clave aleatoria de 64 bits
+IV = get_random_bytes(8) # IV aleatorio de 64 bits para CBC
+BLOCK_SIZE_DES = 8 # Bloque de 64 bits
+data = "Hola amigas de la seguridad".encode("utf-8") # Datos a cifrar
+print(data)
+
+# CIFRADO #######################################################################
+
+# Creamos un mecanismo de cifrado DES en modo CBC con un vector de inicialización IV
+cipher = DES.new(key, DES.MODE_CBC, IV)
+
+# Ciframos, haciendo que la variable “data” sea múltiplo del tamaño de bloque
+ciphertext = cipher.encrypt(pad(data,BLOCK_SIZE_DES))
+print(ciphertext)
+
+# DESCIFRADO #######################################################################
+
+# Creamos un mecanismo de (des)cifrado DES en modo CBC con un vector de
+#inicialización IV para CBC
+# Ambos, cifrado y descifrado, se crean de la misma forma
+decipher_des = DES.new(key, DES.MODE_CBC, IV)
+
+# Desciframos, eliminamos el padding, y recuperamos la cadena
+new_data = unpad(decipher_des.decrypt(ciphertext), BLOCK_SIZE_DES).decode("utf-8",
+"ignore")
+
+# Imprimimos los datos descifrados
+print(new_data)
