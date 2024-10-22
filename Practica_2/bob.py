c5bd5997073a94d2b94162aa69347f6463a0bf03
jcalcausal <jcalcausal@gmail.com>
2024-10-22 19:35:04 +0200

    Practica 2

--- Practica_2/bob.py
+++ Practica_2/bob.py
@@ -3,12 +3,21 @@ import funciones_aes
 from socket_class import *
 from Crypto.Random import get_random_bytes
 
-bob_key = funciones_rsa.cargar_RSAKey_Privada("bob_key.txt")
+bob_key = funciones_rsa.cargar_RSAKey_Privada("bob_key.txt", "Bob")
 alice_pub_key = funciones_rsa.cargar_RSAKey_Publica("alice_pub_key.txt")
 
 #SOCKET SERVIDOR
-socket_servidor = SOCKET_SIMPLE_TCP("127.0.0.1", 1337)
+socket_servidor = SOCKET_SIMPLE_TCP('127.0.0.1', 5511)
 socket_servidor.escuchar()
 cifrado = socket_servidor.recibir()
 firmado = socket_servidor.recibir()
 socket_servidor.cerrar()
+
+#DESCIFRAR Y VERIFICAR FIRMA
+descifrado = funciones_rsa.descifrarRSA_OAEP(cifrado, bob_key)
+print(descifrado)
+try:
+	funciones_rsa.comprobarRSA_PSS(cifrado, firmado, alice_pub_key)
+	print("Verificación de la firma correcta")
+except(ValueError, TypeError):
+	print("Verificación incorrecta de la firma")
